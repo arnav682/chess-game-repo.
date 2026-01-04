7a93e142332c2fc88fcc74c530dca0a339cf57ad
arnav682 <chaudharyusha645@gmail.com>
2026-01-04 18:37:35 +0530

    Initial commit

--- socket.js
+++ socket.js
@@ -122,7 +122,7 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 10000;
-httpServer.listen(PORT,  '0.0.0.0',() => {
+httpServer.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is listening on port ${PORT}`);
 });
 
